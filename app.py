6772971151b3462607bdd9e57857059bdfa5b8dd
akashkumar874 <akashmandru143@gmail.com>
2026-04-06 12:21:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "akash" and password == "1234":
+            return "Login success 🔥"
+        else:
+            return "Login Failed ❌"
+
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
